91ed28cdfa0591be1344081f35a02676768b4d88
Paul English <paul@onfrst.com>
2014-05-25 00:43:47 -0600

    Add some more sanity tests to the coordinates

--- test/gps_sim/utils/coordinates_test.clj
+++ test/gps_sim/utils/coordinates_test.clj
@@ -34,6 +34,25 @@
                              (just [(roughly 107061.5) 40 47 (roughly 19.5) 1 111 49 (roughly 49) -1 (roughly 1874.5)])
                              (just [(roughly 112000.0) 40 48 (roughly 44.0) 1 111 48 (roughly 40) -1 (roughly 2377.0)])])))
 
+    (fact "dms->radians->dms"
+      (radians->dms
+       (dms->radians [[102123.0M 40 45 55.0M 1 111 50 58.0M -1 1372.0M]
+                      [107061.5M 40 47 19.5M 1 111 49 49.0M -1 1874.5M]
+                      [112000.0M 40 48 44.0M 1 111 48 40.0M -1 2377.0M]]))
+      => (just [(just [(roughly 102123.0) 40 45 (roughly 55) 1 111 50 (roughly 58) -1 (roughly 1372)])
+                (just [(roughly 107061.5) 40 47 (roughly 19.5) 1 111 49 (roughly 49) -1 (roughly 1874.5)])
+                (just [(roughly 112000.0) 40 48 (roughly 44) 1 111 48 (roughly 40) -1 (roughly 2377)])]))
+
+    (fact "radians->dms->radians"
+      (dms->radians
+       (radians->dms [[102123.0M 0.7114883177122995M -1.9521410720820354M 1372.0M]
+                      [107061.5M 0.7118979852728371M -1.9518065506420699M 1874.5M]
+                      [112000.0M 0.7123076528333746M -1.9514720292021042M 2377.0M]]))
+      => (just
+          [(just [(roughly 102123) (roughly 0.71148) (roughly -1.9521) (roughly 1372)])
+           (just [(roughly 107061) (roughly 0.71189) (roughly -1.9518) (roughly 1874)])
+           (just [(roughly 112000) (roughly 0.71230) (roughly -1.9514) (roughly 2377)])]))
+
     (fact "rad->cartesian"
       (rad->cartesian [[2M 1M 1M 1M]
                        [3M 1.2M 1M 1M]
@@ -49,4 +68,12 @@
                        [1858826.3947957244M 2894950.586047284M 5358020.046565919M]])
       => (just [(just [(roughly 0) (roughly 1) (roughly 1) (roughly 1)])
                 (just [(roughly 0) (roughly 1.2) (roughly 1) (roughly 1)])
-                (just [(roughly 0) (roughly 1) (roughly 1) (roughly 0.3)])]))))
+                (just [(roughly 0) (roughly 1) (roughly 1) (roughly 0.3)])]))
+
+    (fact "rad->cartesian->rad"
+      (cartesian->rad [2M 3M 2.5M] (rad->cartesian [[2M 1M 1M 1M]
+                                                    [3M 1.2M 1M 1M]
+                                                    [2.5M 1M 1M 0.3M]]))
+      => (just [(just [(roughly 2) (roughly 1) (roughly 1) (roughly 1)])
+                (just [(roughly 3) (roughly 1.2) (roughly 1) (roughly 1)])
+                (just [(roughly 2.5) (roughly 1) (roughly 1) (roughly 0.3)])]))))
